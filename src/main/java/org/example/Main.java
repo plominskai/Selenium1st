2c9eb22aa61c89b677cfb5492758fbdaad06d78e
plominskai <plominska.izabela@gmail.com>
2023-06-11 14:04:06 +0200

    update of the class Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,33 @@
-package org.example;
+//package org.example;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.setProperty("webDriver.Chrome.Driver", "C:\\Users\\plomi\\Selenium\\ChromeDriver\\chromedriver.exe");
+//        WebDriver webDriver = new ChromeDriver();
+//        webDriver.get("https://www.google.com");
+//    }
+//}
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("Webdriver.Chrome.Driver", "C:\\Users\\plomi\\Selenium\\ChromeDriver\\chromedriver.exe");
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.google.com");
+        // Set up browser
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(ops);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Open Google
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+
     }
 }
