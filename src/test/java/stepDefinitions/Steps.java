5e3a9421f65828ca2fc4db222fefbfb5f615e1fa
plominskai <plominska.izabela@gmail.com>
2023-06-15 21:20:51 +0200

    removed further steps from Steps

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -23,37 +23,7 @@ public class Steps {
         // Open webpage
         driver.get("https://demoqa.com/books");
         driver.manage().window().maximize();
-        // HomePage
-        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
-        // LoginPage
-        LoginPage loginPage = PageFactory.initElements(driver,LoginPage.class);
-        // DashboardPage
-        DashboardPage dashboardPage = PageFactory.initElements(driver, DashboardPage.class);
 
-        // click on login button in homepage
-        homePage.loginClick();
-
-        // enter user name
-        loginPage.enterUserName("gunjankaushik");
-
-        //enter password
-        loginPage.enterPassword("Password@123");
-
-        //click on login button
-        loginPage.loginClick();
-
-        // stop for 5 sec
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        // compare logged user with entered user
-        dashboardPage.isCurrentUserCorrect("gunjankaushik");
     }
 }
 
