7f878a7100a84a36fdcd75029407219b968a1a34
plominskai <plominska.izabela@gmail.com>
2023-06-15 17:43:00 +0200

    chromedriver path added

--- src/main/java/org/example/test_case/Main.java
+++ src/main/java/org/example/test_case/Main.java
@@ -13,6 +13,7 @@ import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("Webdriver.Chrome.Driver", "C:\\Users\\plomi\\Selenium\\chromedriver\\chromedriver.exe");
         // Set up browser
         ChromeOptions ops = new ChromeOptions();
         ops.addArguments("--remote-allow-origins=*");
